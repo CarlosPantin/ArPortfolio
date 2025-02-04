3839eefa64eec895beb3dc4d65f65a6f607ed70d
CarlosPantin <carlospantinpang@outlook.com>
2025-02-04 11:30:51 +0200

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Press_Start_2P } from 'next/font/google';
-import './globals.css';
+import React from "react";
+import { Press_Start_2P } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const pressStart2P = Press_Start_2P({
-  subsets: ['latin'],
-  weight: '400',  
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -13,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <head />
       <body className={pressStart2P.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
